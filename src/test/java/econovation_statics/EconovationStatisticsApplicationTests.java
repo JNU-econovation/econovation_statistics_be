5d75edb85e055dd8ff3478116991d52d261fe52d
Jin Hyuk <ekrrdj07@gmail.com>
2024-07-08 20:43:34 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/econovation_statics/EconovationStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package econovation_statics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconovationStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
